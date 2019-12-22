f719f70bb61e36a136f325cff513baf1b3cc3abd
Svetoslav Todorov <svetoslav.todorov@botronsoft.com>
2019-12-22 20:31:13 +0200

    Initial commit.

---
+++ Meme-Platform/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Meme_Platform.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
