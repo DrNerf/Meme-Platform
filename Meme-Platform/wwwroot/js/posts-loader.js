00510d8cbdcfccfa8d4f117a5d3f94c918b4f31c
Svetoslav Todorov <drnerf95@gmail.com>
2020-02-12 23:47:01 +0200

    Added missing js for home page.

---
+++ Meme-Platform/wwwroot/js/posts-loader.js
@@ -0,0 +1,50 @@
+﻿$.fn.isInViewport = function () {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+$(function () {
+    PostsLoader.listenForNextLoad();
+});
+
+PostsLoader = {
+    currentPage: 1,
+    loadNextPage: function () {
+        var self = PostsLoader;
+        self.currentPage++;
+
+        $.get("/Home/Posts?page=" + self.currentPage, function (response) {
+            $("#posts-container").append(response);
+            self.listenForLoadingImages();
+            self.listenForNextLoad();
+        });
+    },
+    listenForLoadingImages: function () {
+        $('.post-image').filter(function () { return !$(this).data('loaded'); }).one('load', function () {
+            var postId = $(this).attr('data-id');
+            $(this).data('loaded', true);
+            if (postId) {
+                var overlay = $('#loading-overlay-' + postId);
+                if (overlay) {
+                    overlay.fadeOut(1000);
+                }
+            }
+        }).each(function () {
+            if (this.complete) $(this).load();
+        });
+    },
+    listenForNextLoad: function () {
+        $(window).on('scroll', function (event) {
+            if ($('.progress-spinner').isInViewport()) {
+                console.log("Reached end of posts page, loading more.");
+                $(this).off('scroll');
+                PostsLoader.loadNextPage();
+            }
+        });
+    }
+};
+
+PostsLoader.listenForLoadingImages();
---
+++ Meme-Platform/wwwroot/js/voting-functions.js
@@ -0,0 +1,85 @@
+﻿var upVoteUrl = "/Posts/Vote";
+var unvoteUrl = "/Posts/Unvote";
+var isInChitChatMode = false;
+
+function vote(postId, type) {
+    var isUpvote = type > 0;
+    var button = $('#' + (isUpvote ? 'upvote-' : 'downvote-') + postId);
+    if (button.hasClass('disabled')) {
+        return;
+    }
+
+    var twinButton = $('#' + (!isUpvote ? 'upvote-' : 'downvote-') + postId);
+    var isUnvote = button.attr('data-unvote').toLowerCase() == 'true';
+    var voteUrl = isUnvote ? unvoteUrl : upVoteUrl;
+    var data = {
+        postId: postId,
+        chitChatId: postId,
+        voteType: type
+    };
+
+    $.post(voteUrl, data);
+
+    if (isInChitChatMode) {
+        if (!isUnvote) {
+            button.removeClass('btn-danger');
+            button.removeClass('btn-success');
+            button.addClass('btn-primary');
+            twinButton.addClass('disabled');
+            button.attr('data-unvote', 'True');
+        } else {
+            button.removeClass('btn-success');
+            twinButton.removeClass('disabled');
+            if (isUpvote) {
+                button.addClass('btn-success');
+            } else {
+                button.addClass('btn-danger');
+            }
+            button.attr('data-unvote', 'False');
+            twinButton.attr('data-unvote', 'False');
+        }
+    } else {
+        if (!isUnvote) {
+            button.removeClass('btn-primary');
+            if (isUpvote) {
+                button.addClass('btn-success');
+                twinButton.addClass('disabled');
+            } else {
+                button.addClass('btn-danger');
+                twinButton.addClass('disabled');
+            }
+            button.attr('data-unvote', 'True');
+
+        } else {
+            if (isUpvote) {
+                button.removeClass('btn-success');
+                twinButton.removeClass('disabled');
+            } else {
+                button.removeClass('btn-danger');
+                twinButton.removeClass('disabled');
+            }
+            button.addClass('btn-primary');
+            button.attr('data-unvote', 'False');
+            twinButton.attr('data-unvote', 'False');
+        }
+    }
+
+
+    correctScore(postId, isUpvote, isUnvote);
+}
+
+function correctScore(postId, isUpvote, isUnvote) {
+    var scoreElement = $('#score-' + postId);
+    var currentScore = parseInt(scoreElement.text());
+    var corrector = isUpvote ? 1 : -1;
+    if (isUnvote) {
+        corrector *= -1;
+    }
+    scoreElement.text(currentScore + corrector);
+}
+
+function configureVoting(upVoteUrlParam, unvoteUrlParam, isInChitChatModeParam) {
+    upVoteUrl = upVoteUrlParam;
+    unvoteUrl = unvoteUrlParam;
+    isInChitChatMode = isInChitChatModeParam;
+}
